76b4004e751fef1d3e2294c0f59dc325564c32bf
Alysson Oliveira <alysson00637@gmail.com>
2023-06-13 22:07:37 -0300

    tradução e atualização

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Sua senha foi redefinida!',
+    'sent' => 'Enviamos um email com o link para redefinição de sua senha!',
+    'throttled' => 'Por favor, aguarda para tentar novamente.',
+    'token' => 'Código de validação inválido!.',
+    'user' => "Não localizamos um usuário com esse email!",
 
 ];
--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,8 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
+
+    <div {{ $attributes->merge(['class' => 'font-medium text-sm bg-green-100 border border-green-400 text-green-700 mb-3 px-4 py-3 rounded relative', 'role' => 'alert' ])}}>
         {{ $status }}
     </div>
 @endif
